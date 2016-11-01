26989d4f89fd6db10ff179a7ac301fc4c2cdb803
Nich0l4s <sfsixbot@gmail.com>
2016-11-01 23:36:48 +0800

    1.修改：现在使用MongoDB数据库存储爬取信息而不在采用文件的形式

--- LaGou.py
+++ LaGou.py
@@ -2,11 +2,13 @@
 # encoding=utf-8
 import requests
 import json
-import csv
 import os
+import datetime
+from pymongo import MongoClient
 from bs4 import BeautifulSoup
 
 
+MONGO_CONN = MongoClient('127.0.0.1', 27017)
 DOWNLOAD_URL = 'http://www.lagou.com/jobs/list_Python?px=default&city=%E5%85%A8%E5%9B%BD#filterBox'
 INFO_URL = 'http://www.lagou.com/jobs/positionAjax.json?px=default&needAddtionalResult=false'
 LOGO_URL = 'http://www.lgstatic.com/thumbnail_120x120/'
@@ -34,35 +36,50 @@ def get_total_page(url):
     return total
 
 
+def save_data(data):
+    data['_id'] = data['positionId']
+    data['updateTime'] = datetime.datetime.now()
+    MONGO_CONN['myDB']['LaGou'].update_one(
+        filter={'_id': data['_id']},
+        update={'$set': data},
+        upsert=True
+    )
+
+
 def main():
     total = get_total_page(DOWNLOAD_URL)
     if not os.path.exists(LOGO_PATH):
         os.mkdir(LOGO_PATH)
-    with open('LaGouPython.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['职位', '薪水', '工作地点', '公司名称', '工作时间', '教育程度'])
-        for index in range(1, total + 1):
+    for index in range(1, total + 1):
+        data = {
+            'first': 'false',
+            'pn': str(index),
+            'kd': 'Python'
+        }
+        json_obj = post_form(INFO_URL, data=data)
+        for info in json_obj['content']['positionResult']['result']:
+            name = LOGO_PATH + info['companyLogo'].split('/')[-1]
+            mark = name.find('?')
+            if mark != -1:
+                name = name[:mark]
+            if os.path.exists(name):
+                pass
+            else:
+                logo = requests.get(LOGO_URL + info['companyLogo']).content
+                with open(name, 'wb') as lf:
+                    lf.write(logo)
             data = {
-                'first': 'false',
-                'pn': str(index),
-                'kd': 'Python'
+                'positionId': info['positionId'],
+                'positionName': info['positionName'],
+                'salary': info['salary'],
+                'city': info['city'],
+                'businessZones': info['businessZones'],
+                'companyFullName': info['companyFullName'],
+                'workYear': info['workYear'],
+                'education': info['education'],
+                'logoPath': name
             }
-            json_obj = post_form(INFO_URL, data=data)
-            for info in json_obj['content']['positionResult']['result']:
-                data = [info['positionName'], info['salary'], info['city'] +
-                        ('' if info['businessZones'] is None else '·' + info['businessZones'][0]),
-                        info['companyFullName'], info['workYear'], info['education']]
-                writer.writerow(data)
-                name = LOGO_PATH + info['companyLogo'].split('/')[-1]
-                mark = name.find('?')
-                if mark != -1:
-                    name = name[:mark]
-                if os.path.exists(name):
-                    pass
-                else:
-                    logo = requests.get(LOGO_URL + info['companyLogo']).content
-                    with open(name, 'wb') as lf:
-                        lf.write(logo)
+            save_data(data)
 
 
 if __name__ == '__main__':
